376849de3a3009e10d27c5095d10832ae0a27c28
Miguel Loureiro <loureirooliveiram@gmail.com>
2026-03-02 08:34:09 -0300

    tirei o botão certo agora

--- frontend/src/pages/Settings/Settings.jsx
+++ frontend/src/pages/Settings/Settings.jsx
@@ -32,19 +32,19 @@ export default function Settings() {
       label: translate('Company Settings'),
       icon: <TrophyOutlined />,
       children: <CompanySettings />,
-    },
+    },/*
     {
       key: 'company_logo',
       label: translate('Company Logo'),
       icon: <FileImageOutlined />,
       children: <CompanyLogoSettings />,
-    },/*
+    },*/
     {
       key: 'currency_settings',
       label: translate('Currency Settings'),
       icon: <DollarOutlined />,
       children: <MoneyFormatSettings />,
-    },*/
+    },
     {
       key: 'finance_settings',
       label: translate('Finance Settings'),
